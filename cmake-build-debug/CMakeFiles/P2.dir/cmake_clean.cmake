1744023a075e6f5e05fede1cfc83503ece204a3b
nambles <crzyhunter@gmail.com>
2018-02-26 18:32:52 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/P2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P2.dir/Complex.cc.obj"
+  "CMakeFiles/P2.dir/InputImage.cc.obj"
+  "CMakeFiles/P2.dir/fft2d.cc.obj"
+  "P2.pdb"
+  "P2.exe"
+  "P2.exe.manifest"
+  "libP2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
